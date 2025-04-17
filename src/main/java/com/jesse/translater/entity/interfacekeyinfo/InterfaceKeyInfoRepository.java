0d5f6174efd09afa49ebd82003768bca978f7b95
浩进 朱 <3191955858@qq.com>
2025-04-17 11:01:24 +0800

    接口信息实体仓库类

---
+++ src/main/java/com/jesse/translater/entity/interfacekeyinfo/InterfaceKeyInfoRepository.java
@@ -0,0 +1,6 @@
+package com.jesse.translater.entity.interfacekeyinfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterfaceKeyInfoRepository
+                    extends JpaRepository<InterfaceKeyInfoEntity, String> {}
