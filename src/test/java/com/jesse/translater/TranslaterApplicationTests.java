bbebff2d78ca62b2110fa5531b707d65e294b662
浩进 朱 <3191955858@qq.com>
2025-04-17 11:02:44 +0800

    Spring Web 应用测试用例类（暂无）

---
+++ src/test/java/com/jesse/translater/TranslaterApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jesse.translater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.*;
+import java.util.Base64;
+
+@SpringBootTest
+class TranslaterApplicationTests
+{
+}
